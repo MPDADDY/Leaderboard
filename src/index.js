378f0b59136657951b0822da9083f6b77c7509ee
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-09-06 16:12:37 +0300

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
-import _ from 'lodash';
 import './style.css';
 
-let data = [
+const data = [
   {
-    name: "Amina",
-    score: 30
+    name: 'Amina',
+    score: 30,
   },
   {
-    name: "Kagome",
-    score: 300
+    name: 'Kagome',
+    score: 300,
   },
   {
-    name: "Waweru",
-    score: 50
+    name: 'Waweru',
+    score: 50,
   },
   {
-    name: "Abujs",
-    score: 10
+    name: 'Abujs',
+    score: 10,
   },
   {
-    name: "Joe",
-    score: 130
+    name: 'Joe',
+    score: 130,
   },
   {
-    name: "Bob",
-    score: 100
+    name: 'Bob',
+    score: 100,
   },
-]
+];
 
-function render (data) {
-  let article = document.getElementById('leaderboard');
-  for (let i = 0; i < data.length; i+= 1) {
-    let name = data[i].name;
-    let score = data[i].score;
-    let myP = document.createElement('p');
-    myP.innerHTML = `${ name }: ${ score }`
+function render(data) {
+  const article = document.getElementById('leaderboard');
+  for (let i = 0; i < data.length; i += 1) {
+    const { name } = data[i];
+    const { score } = data[i];
+    const myP = document.createElement('p');
+    myP.innerHTML = `${name}: ${score}`;
     article.appendChild(myP);
   }
 }
 
-function addClass () {
-  let paragraphs = document.querySelectorAll('#leaderboard p');
+function addClass() {
+  const paragraphs = document.querySelectorAll('#leaderboard p');
   for (let i = 0; i < paragraphs.length; i += 1) {
     if (i % 2 === 0) {
       paragraphs[i].classList.add('gray');
