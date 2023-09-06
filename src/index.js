3f03eef216ac088c6a8633843adca4a5363f54eb
Anold Mwagharo <anoldmwawasi@gmail.com>
2023-09-06 15:59:33 +0300

    Add js for rendering recent scores

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+let data = [
+  {
+    name: "Amina",
+    score: 30
+  },
+  {
+    name: "Kagome",
+    score: 300
+  },
+  {
+    name: "Waweru",
+    score: 50
+  },
+  {
+    name: "Abujs",
+    score: 10
+  },
+  {
+    name: "Joe",
+    score: 130
+  },
+  {
+    name: "Bob",
+    score: 100
+  },
+]
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+function render (data) {
+  let article = document.getElementById('leaderboard');
+  for (let i = 0; i < data.length; i+= 1) {
+    let name = data[i].name;
+    let score = data[i].score;
+    let myP = document.createElement('p');
+    myP.innerHTML = `${ name }: ${ score }`
+    article.appendChild(myP);
+  }
 }
 
-document.body.appendChild(component());
+function addClass () {
+  let paragraphs = document.querySelectorAll('#leaderboard p');
+  for (let i = 0; i < paragraphs.length; i += 1) {
+    if (i % 2 === 0) {
+      paragraphs[i].classList.add('gray');
+    }
+  }
+}
+render(data);
+addClass();
